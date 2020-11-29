5e5fd3802775bbb5a6814ae98dfdac23fe681579
vinayaka-17 <75211643+vinayaka-17@users.noreply.github.com>
2020-11-29 22:04:10 +0530

    Add files via upload

---
+++ lab2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+{
+public static void main(String args[])
+{
+int a=5;
+int b=7;
+System.out.println("a&b="+(a&b));
+System.out.println("a/b="+(a/b));
+System.out.println("a^b="+(a^b));
+System.out.println("~a="+~a);
+a&=b;
+System.out.println("a="+a);
+}
+}
+}
